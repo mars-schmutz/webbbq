0810533e3209c4ed2bda869ce5c16b8454356eeb
Marshall Schmutz <darthmars97@gmail.com>
2024-04-09 13:09:30 -0600

    upload client

---
+++ client/src/App.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react"
+import './App.css'
+
+function Button({ display, msg, style, url }) {
+    const handleClick = () => {
+        fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ message: msg })
+        })
+    }
+
+    return (
+        <button
+            className={`button ${style}`}
+            onClick={handleClick}
+        >
+            {display}
+        </button>
+    )
+}
+
+function App() {
+    const [urlInput, setUrlInput] = useState("");
+
+    const handleUrlChange = (event) => {
+        setUrlInput(event.target.value);
+    }
+
+    return (
+        <>
+            <input type="text" value={urlInput} onChange={handleUrlChange} />
+            <div class="response-btns">
+                <Button msg={"good"} style="good" display="Good" />
+                <Button msg={"slow"} style="slow" display="Slow" />
+                <Button msg={"stop"} style="stop" display="Stop" />
+            </div>
+        </>
+    )
+}
+
+export default App
