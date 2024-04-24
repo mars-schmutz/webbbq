5361af67081e433a0c992f3f84aa2e71a8dd96b9
Marshall Schmutz <darthmars97@gmail.com>
2024-04-24 09:23:47 -0600

    add basic server password for teacher

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
+import hashlib
 
 
 app = FastAPI()
@@ -12,18 +13,31 @@ app.add_middleware(
     allow_headers = ["*"]
 )
 
+PASSWD = "c963ca56d7ee4d9ef16e856f2d47cb148acc9618d6c401eccb391bdea0dd8dd2"
+
 updates: list[dict[str, str]] = []
+
+def passwd_check(pwd):
+    bytestr = pwd.encode("utf-8")
+    hashed = hashlib.sha256(bytestr).hexdigest()
+    return hashed
 
 @app.post("/bbbq", status_code = 201)
 async def update_status(message: dict) -> None:
     updates.append(message)
 
 @app.get("/teacher")
-async def update_teacher() -> dict:
+async def update_teacher(req: Request) -> dict:
+    header = "X-TotallySecure"
+    if header not in req.headers or passwd_check(req.headers[header]) != PASSWD:
+        raise HTTPException(status_code = 401, detail = "Unauthorized")
     return {
         "updates": updates
     }
 
 @app.post("/reset", status_code = 200)
-async def clear_updates() -> None:
+async def clear_updates(req: Request) -> None:
+    header = "X-TotallySecure"
+    if header not in req.headers or passwd_check(req.headers[header]) != PASSWD:
+        raise HTTPException(status_code = 401, detail = "Unauthorized")
     updates.clear()
--- teacher/src/App.tsx
+++ teacher/src/App.tsx
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from "react"
 import './App.css'
 
-function UpdateWindow({ url }) {
+function UpdateWindow({ url, passwd }) {
   const [updates, setUpdates] = useState([]);
 
   useEffect(() => {
     const fetchUpdates = async () => {
       try {
-        //const resp = await fetch("http://localhost:8000/teacher")
-        const _resp = await fetch(`${url}/teacher`)
-        const resp = await fetch("https://webbbq.onrender.com/teacher")
+        //const _resp = await fetch(`${url}/teacher`)
+        const headers = {
+          "X-TotallySecure": passwd
+        }
+        const resp = await fetch("https://webbbq.onrender.com/teacher", {
+          method: "GET",
+          headers: headers
+        })
         const data = await resp.json()
         setUpdates(data.updates.reverse())
       } catch (err) {
@@ -19,18 +24,20 @@ function UpdateWindow({ url }) {
 
     fetchUpdates();
 
-    const timer = setInterval(fetchUpdates, 1000)
+    const timer = setInterval(fetchUpdates, 100)
 
     return () => clearInterval(timer)
-  }, [])
+  }, [passwd])
 
   const handleReset = () => {
     fetch("https://webbbq.onrender.com/reset", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        "X-TotallySecure": passwd
       }
     })
+    console.log(`passwd: ${passwd}`);
     setUpdates([])
   }
 
@@ -49,14 +56,20 @@ function UpdateWindow({ url }) {
 
 function App() {
   const [urlInput, setUrlInput] = useState("");
+  const [passwdInput, setPasswdInput] = useState("");
 
   const handleUrlChange = (event) => {
     setUrlInput(event.target.value);
   }
 
+  const handlePasswdChange = (event) => {
+    setPasswdInput(event.target.value);
+  }
+
   return (
     <>
-      <UpdateWindow url={urlInput} />
+      <input type="password" value={passwdInput} onChange={handlePasswdChange} />
+      <UpdateWindow url={urlInput} passwd={passwdInput} />
     </>
   )
 }
